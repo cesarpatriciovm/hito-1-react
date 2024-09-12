88867bab0f9e5c22563f8462def9384f51117608
cesarpatriciovm <cesarvalladaresmilla@gmail.com>
2024-09-12 10:40:20 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
+import Home from './component/Home';
+
+function App() {
+  
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
+export default App
